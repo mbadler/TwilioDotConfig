97ed48c934196ae117a06f5adc3e2fd2724664a8
mbadler <mbadler@thejnet.com>
2013-12-23 22:35:45 -0500

    Initial Commit

---
+++ TwilioDotConfigClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Twilio;
+using RestSharp;
+using System.Reflection;
+
+namespace TwilioDotConfig
+{
+    public class TwilioDotConfigClient:TwilioRestClient
+    {
+
+        static TwilioDotConfigSection config =
+        (TwilioDotConfigSection)System.Configuration.ConfigurationManager.GetSection(
+        "TwilioDotNet");
+
+        public TwilioDotConfigClient(string accountSid, string authToken) : this(accountSid, authToken, accountSid) { }
+
+        public TwilioDotConfigClient(string accountSid, string authToken, string accountResourceSid)
+            : base(accountSid, authToken, accountResourceSid)
+        {
+
+        }
+
+        public TwilioDotConfigClient()
+            : base(config.accountSid,config. authToken,config.accountResourceSid)
+        {
+            if (config.TwilioBaseURL != "")
+            {
+                // setup the base url using reflection
+                Type type = typeof (TwilioRestClient) ;
+                var b = (RestClient)type.GetField("_client", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this);
+                b.BaseUrl = config.TwilioBaseURL;
+            }
+        }
+    }
+}
---
+++ TwilioDotConfigSection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace TwilioDotConfig
+{
+    public class TwilioDotConfigSection:ConfigurationSection
+    {
+        [ConfigurationProperty("accountSid", DefaultValue = "", IsRequired = false)]
+        public string accountSid
+        {
+            get { return (string)this["accountSid"]; }
+            set { this["accountSid"] = value; }
+        }
+
+        [ConfigurationProperty("authToken", DefaultValue = "", IsRequired = false)]
+        public string authToken
+        {
+            get { return (string)this["authToken"]; }
+            set { this["authToken"] = value; }
+        }
+
+        [ConfigurationProperty("accountResourceSid", DefaultValue = "", IsRequired = false)]
+        public string accountResourceSid
+        {
+            get { return (string)this["accountResourceSid"]; }
+            set { this["accountResourceSid"] = value; }
+        }
+
+        [ConfigurationProperty("TwilioBaseURL", DefaultValue = "https://api.twilio.com/", IsRequired = false)]
+        public string TwilioBaseURL
+        {
+            get { return (string)this["TwilioBaseURL"]; }
+            set { this["TwilioURL"] = value; }
+        }
+    }
+}
